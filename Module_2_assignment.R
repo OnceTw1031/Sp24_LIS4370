8c5e6cae8db46e40bceac61380c99f04dbbb5a10
OnceTw1031 <156630776+OnceTw1031@users.noreply.github.com>
2024-01-21 20:29:27 -0500

    Update Module_2_assignment.R

--- Module_2_assignment.R
+++ Module_2_assignment.R
@@ -2,6 +2,6 @@ install.packages("ISwR")
 library(ISwR)
 
 
-assignment2<- c(6,18,14,22,27,17,22,20,22)
+assignment2<- c(16,18,14,22,27,17,19,17,17,22,20,22)
 myMean <- function(assignment2) {return(sum(assignment2)/length(assignment2))}
 myMean(assignment2)
