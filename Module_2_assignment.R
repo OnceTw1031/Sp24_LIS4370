52cda819c978570eadbd010e311a28a45413bafe
OnceTw1031 <156630776+OnceTw1031@users.noreply.github.com>
2024-01-21 20:19:00 -0500

    Add files via upload

---
+++ Module_2_assignment.R
@@ -0,0 +1,7 @@
+install.packages("ISwR")
+library(ISwR)
+
+
+assignment2<- c(6,18,14,22,27,17,22,20,22)
+myMean <- function(assignment2) {return(sum(assignment2)/length(assignment2))}
+myMean(assignment2)
